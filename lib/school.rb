1b22e06411621f09430a7d3b83f4785791660cec
John Souza <souzajohnlee@gmail.com>
2020-03-10 21:56:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,5 +2,13 @@
 
 class School
   
+  attr_reader :roster
+  
   def initialize(name)
-    
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    @roster[student name] = grade
+  end
